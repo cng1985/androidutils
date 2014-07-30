07d76fee54eb390f7cc2c6273da7eb02958b4a81
ada.young <cng1985@gmail.com>
2014-07-30 10:03:09 +0800

    新增不可以滚动的GridView

---
+++ src/com/ada/widget/MyGridView.java
@@ -0,0 +1,32 @@
+package com.ada.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+/**
+ * 不可以滚动的GridView
+ * @author 年高
+ *
+ */
+public class MyGridView extends GridView {
+
+	public MyGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public MyGridView(Context context) {
+		super(context);
+	}
+
+	public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	// 该自定义控件只是重写了GridView的onMeasure方法，使其不会出现滚动条，ScrollView嵌套ListView也是同样的道理，不再赘述。
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+				MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+}
