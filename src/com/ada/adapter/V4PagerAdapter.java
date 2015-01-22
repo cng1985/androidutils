f9aa612ff76d7bae28611739c9bd88719717772d
ada.young <cng1985@gmail.com>
2015-01-22 13:14:19 +0800

    新建pageradapter

---
+++ src/com/ada/adapter/V4PagerAdapter.java
@@ -0,0 +1,42 @@
+package com.ada.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class V4PagerAdapter   extends FragmentPagerAdapter {
+
+	public V4PagerAdapter(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Fragment> fragments = new ArrayList<Fragment>();
+	public List<String> titles = new ArrayList<String>();
+
+
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return fragments.get(arg0);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return fragments.size();
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return titles.get(position);
+	}
+
+	public void add(String title, Fragment fragment) {
+		fragments.add(fragment);
+		titles.add(title);
+	}
+}
---
+++ src/com/ada/fragment/BaseFragment.java
@@ -0,0 +1,48 @@
+package com.ada.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+
+	public abstract int getlayout();
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(getlayout(), container, false);
+		return rootView;
+	}
+
+	/**
+	 * 初始化方法
+	 * 
+	 * @param savedInstanceState
+	 */
+	public abstract void create(Bundle savedInstanceState);
+
+	private View view;
+
+	/**
+	 * 根据id查找控件
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public final View findViewById(int id) {
+		return view.findViewById(id);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		if (getView() != null) {
+			view = getView();
+			create(savedInstanceState);
+		}
+	}
+}
